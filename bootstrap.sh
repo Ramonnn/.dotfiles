deb37261e28f77f6e4ee2b94295e67d06842fb06
Ramonnn <29675067+Ramonnn@users.noreply.github.com>
2024-01-16 22:12:05 +0100

    added github to known hosts

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,17 +5,18 @@ ROOTDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 # Host file location
 HOSTS="$ROOTDIR/hosts"
 # Playbook 1
-PLAYBOOK_1="$ROOTDIR/dotfiles_1.yml"
+PLAYBOOK_1="$ROOTDIR/dotfiles.yml"
 # Playbook 2
-PLAYBOOK_2="$ROOTDIR/dotfiles_2.yml"
+PLAYBOOK_2="$ROOTDIR/dotfiles.yml"
 
 # Installs ansible
 sudo apt-get update && sudo apt-get install -y ansible
 
-if [ -e "~/.ssh/github_ssh"]; then 
+if [ -e "~/.ssh/github_ssh" ]; then 
   echo "ssh file already exists."  
 else
-  ssh-keygen -t ed25519 -C "29675067+Ramonnn@users.noreply.github.com" -f ~/.ssh/github_ssh -q -N ""
+  ssh-keygen -t ed25519 -C "" -f ~/.ssh/github_ssh -q -N ""
+    repo: "git@github.com:Ramonnn/lazy-nvim-setup.git"
   eval "$(ssh-agent -s)"
   ssh-add ~/.ssh/github_ssh
 fi
@@ -25,10 +26,9 @@ ansible-playbook -vvv -i "$HOSTS" "$PLAYBOOK_1" --ask-become-pass
 
 # Prompt the public ssh key
 cat ~/.ssh/github_ssh.pub
-echo "Add the public SSH key to your GitHub Account. Press Enter to continue..."
+echo "Add the public key to your GitHub account. Press Enter to continue..."
 read -r
 
 # Runs the second Ansible playbook using our user.
 ansible-playbook -vvv -i "$HOSTS" "$PLAYBOOK_2" --ask-become-pass
-
 exit 0
