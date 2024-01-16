3d162c2ca53dcecfb1ff8b2641d6f31e23f1ffaf
Ramonnn <29675067+Ramonnn@users.noreply.github.com>
2024-01-16 22:17:11 +0100

    added github to known hosts

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,20 +5,16 @@ ROOTDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 # Host file location
 HOSTS="$ROOTDIR/hosts"
 # Playbook 1
-PLAYBOOK_1="$ROOTDIR/dotfiles.yml"
+PLAYBOOK_1="$ROOTDIR/dotfiles_1.yml"
 # Playbook 2
-PLAYBOOK_2="$ROOTDIR/dotfiles.yml"
+PLAYBOOK_2="$ROOTDIR/dotfiles_2.yml"
 
 # Installs ansible
 sudo apt-get update && sudo apt-get install -y ansible
 
-if [ -e "~/.ssh/github_ssh" ]; then 
-  echo "ssh file already exists."  
-else
-  ssh-keygen -t ed25519 -C "git@github.com:Ramonnn/lazy-nvim-setup.git" -f ~/.ssh/github_ssh -q -N ""
-  eval "$(ssh-agent -s)"
-  ssh-add ~/.ssh/github_ssh
-fi
+ssh-keygen -t ed25519 -C "git@github.com:Ramonnn/lazy-nvim-setup.git" -f ~/.ssh/github_ssh -q -N ""
+eval "$(ssh-agent -s)"
+ssh-add ~/.ssh/github_ssh
 
 # Runs the first Ansible playbook using our user.
 ansible-playbook -vvv -i "$HOSTS" "$PLAYBOOK_1" --ask-become-pass
